1f93fc0a595e4dfef15132dcd8a95179fff5e86b
gilson2022 <gilson.aparecido.rodrigues@gmail.com>
2022-04-14 18:36:40 -0400

    Commit inicial

---
+++ src/main/java/br/com/gartech/bootstore/BootstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.gartech.bootstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gartech/bootstore/BootstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gartech.bootstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
