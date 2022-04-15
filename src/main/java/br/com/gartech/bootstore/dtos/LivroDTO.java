500920137b23d4074a46b9cc8243f01d6205a684
gilson2022 <gilson.aparecido.rodrigues@gmail.com>
2022-04-15 02:42:25 -0400

    PUT e PATCH para Livro

---
+++ src/main/java/br/com/gartech/bootstore/dtos/LivroDTO.java
@@ -0,0 +1,36 @@
+package br.com.gartech.bootstore.dtos;
+
+import br.com.gartech.bootstore.domain.Livro;
+
+import java.io.Serializable;
+
+public class LivroDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+    private String titulo;
+
+    public LivroDTO() {
+    }
+
+    public LivroDTO(Livro obj) {
+        this.id = obj.getId();
+        this.titulo = obj.getTitulo();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
--- src/main/java/br/com/gartech/bootstore/resources/LivroResource.java
+++ src/main/java/br/com/gartech/bootstore/resources/LivroResource.java
@@ -1,13 +1,14 @@
 package br.com.gartech.bootstore.resources;
 
 import br.com.gartech.bootstore.domain.Livro;
+import br.com.gartech.bootstore.dtos.LivroDTO;
 import br.com.gartech.bootstore.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/livros")
@@ -20,5 +21,24 @@ public class LivroResource {
     public ResponseEntity<Livro> findById(@PathVariable Integer id) {
         Livro obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+//    @GetMapping
+//    public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat) {
+////        List<Livro> list = service.findAll(id_cat);
+////        List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+//        return ResponseEntity.ok().body();
+//    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
+    }
+
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+        Livro newObj = service.update(id, obj);
+        return ResponseEntity.ok().body(newObj);
     }
 }
