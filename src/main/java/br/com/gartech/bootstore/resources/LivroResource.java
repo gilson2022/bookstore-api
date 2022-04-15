834b93ecfb79fac8598977c36030ef4f860fa30a
gilson2022 <gilson.aparecido.rodrigues@gmail.com>
2022-04-15 02:52:07 -0400

    POST para livro

--- src/main/java/br/com/gartech/bootstore/resources/LivroResource.java
+++ src/main/java/br/com/gartech/bootstore/resources/LivroResource.java
@@ -6,7 +6,10 @@ import br.com.gartech.bootstore.service.LivroService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,4 +44,12 @@ public class LivroResource {
         Livro newObj = service.update(id, obj);
         return ResponseEntity.ok().body(newObj);
     }
+
+    @PostMapping
+    public ResponseEntity<Livro> create(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat, @RequestBody Livro obj) {
+        Livro newObj = service.create(id_cat, obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/livros/{id}").buildAndExpand(newObj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/br/com/gartech/bootstore/service/LivroService.java
+++ src/main/java/br/com/gartech/bootstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package br.com.gartech.bootstore.service;
 
+import br.com.gartech.bootstore.domain.Categoria;
 import br.com.gartech.bootstore.domain.Livro;
 import br.com.gartech.bootstore.domain.repositories.LivroRepository;
 import br.com.gartech.bootstore.service.exceptions.ObjectNotFoundException;
@@ -39,5 +40,12 @@ public class LivroService {
         newObj.setTitulo(obj.getTitulo());
         newObj.setNome_autor(obj.getNome_autor());
         newObj.setTexto(obj.getTexto());
+    }
+
+    public Livro create(Integer id_cat, Livro obj) {
+        obj.setId(null);
+        Categoria cat = categoriaService.findById(id_cat);
+        obj.setCategoria(cat);
+        return repository.save(obj);
     }
 }
