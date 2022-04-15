49c57124254c48bcac610f1ccabf8a605d41743d
gilson2022 <gilson.aparecido.rodrigues@gmail.com>
2022-04-14 23:09:08 -0400

    foi implementado o endpoint categorias com o metodo findById na camada de CategoriaService

---
+++ src/main/java/br/com/gartech/bootstore/resources/CategoriaResource.java
@@ -0,0 +1,21 @@
+package br.com.gartech.bootstore.resources;
+
+import br.com.gartech.bootstore.domain.Categoria;
+import br.com.gartech.bootstore.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+        Categoria obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
